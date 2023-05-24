e49c479fea098c24b9fd55265700a04664ad1ba9
viktorija-152 <134477382+viktorija-152@users.noreply.github.com>
2023-05-24 09:07:12 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    images.iconImage(IconNames.Heart).showImage(0)
+    music.playMelody("E B C5 A B G A F ", 231)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Skull)
+    music.playMelody("E B C5 A B G A F ", 231)
+})
